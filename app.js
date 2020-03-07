48331abe3ee22d0d184e910c9a0c8a79d273ffc6
Stella Tran <stellatranthi@hotmail.com>
2020-03-07 13:00:20 -0800

    First commit

---
+++ app.js
@@ -0,0 +1,20 @@
+let express = require('express');
+let app = express();
+let bodyParser = require('body-parser');
+let path = require('path');
+let staticPath = express.static(path.join(__dirname,'public'));
+
+var port = process.env.PORT || 8000;
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname,'views','index.html'));
+});
+
+app.listen(port, function() {
+	console.log('Listening on http://localhost:' + port);
+});
